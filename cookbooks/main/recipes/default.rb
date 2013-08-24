72887b66ae4b6fdc5a9c06e20d0d5fd4e3034c8e
niedhui <dianhui.nie@gmail.com>
2013-08-24 14:49:59 +0800

    gemrc for deployer

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -1,10 +1,21 @@
 package 'git-core'
+package 'vim'
 
 include_recipe "rbenv::default"
 include_recipe "rbenv::ruby_build"
+
 rbenv_ruby '2.0.0-p247' do
   global true
 end
+rbenv_gem 'ruby-shadow' do
+  source 'http://ruby.taobao.org'
+end
+rbenv_gem 'bundler' do
+  source 'http://ruby.taobao.org'
+end
+
+execute "gem sources --remove http://rubygems.org"
+execute "gem sources --add http://ruby.taobao.org"
 
 user node[:user][:name] do
   password node[:user][:password]
@@ -14,3 +25,7 @@ user node[:user][:name] do
   shell "/bin/bash"
 end
 
+template "/home/#{node[:user][:name]}/.gemrc" do
+  source "gemrc"
+  owner node[:user][:name]
+end
