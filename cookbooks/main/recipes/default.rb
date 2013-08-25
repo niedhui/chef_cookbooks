549ee93dc5e4460ad2abfedc1b545ceb03adb48f
niedhui <dianhui.nie@gmail.com>
2013-08-25 22:34:04 +0800

    nginx recipe

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -1,6 +1,22 @@
 package 'git-core'
 package 'vim'
 
+# create deployer user
+user node[:user][:name] do
+  password node[:user][:password]
+  gid 'admin'
+  home "/home/#{node[:user][:name]}"
+  supports manage_home: true
+  shell "/bin/bash"
+end
+
+template "/home/#{node[:user][:name]}/.gemrc" do
+  source "gemrc"
+  owner node[:user][:name]
+end
+
+
+# install rbenv
 include_recipe "rbenv::default"
 include_recipe "rbenv::ruby_build"
 
@@ -17,15 +33,5 @@ end
 execute "gem sources --remove http://rubygems.org"
 execute "gem sources --add http://ruby.taobao.org"
 
-user node[:user][:name] do
-  password node[:user][:password]
-  gid 'admin'
-  home "/home/#{node[:user][:name]}"
-  supports manage_home: true
-  shell "/bin/bash"
-end
-
-template "/home/#{node[:user][:name]}/.gemrc" do
-  source "gemrc"
-  owner node[:user][:name]
-end
+# install nginx
+include_recipe "nginx::source"
