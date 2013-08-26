9332e8cbeaaac6dc562f31f466bbada12c4cbb73
niedhui <dianhui.nie@gmail.com>
2013-08-26 15:19:14 +0800

    add iptables

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -3,16 +3,16 @@ package 'vim'
 
 # create deployer user
 user node[:user][:name] do
-  password node[:user][:password]
-  gid 'admin'
-  home "/home/#{node[:user][:name]}"
-  supports manage_home: true
-  shell "/bin/bash"
+password node[:user][:password]
+gid 'admin'
+home "/home/#{node[:user][:name]}"
+supports manage_home: true
+shell "/bin/bash"
 end
 
 template "/home/#{node[:user][:name]}/.gemrc" do
-  source "gemrc"
-  owner node[:user][:name]
+source "gemrc"
+owner node[:user][:name]
 end
 
 
@@ -21,13 +21,13 @@ include_recipe "rbenv::default"
 include_recipe "rbenv::ruby_build"
 
 rbenv_ruby '2.0.0-p247' do
-  global true
+global true
 end
 rbenv_gem 'ruby-shadow' do
-  source 'http://ruby.taobao.org'
+source 'http://ruby.taobao.org'
 end
 rbenv_gem 'bundler' do
-  source 'http://ruby.taobao.org'
+source 'http://ruby.taobao.org'
 end
 
 execute "gem sources --remove http://rubygems.org"
@@ -35,3 +35,34 @@ execute "gem sources --add http://ruby.taobao.org"
 
 # install nginx
 include_recipe "nginx::source"
+
+
+## iptables
+include_recipe "simple_iptables::default"
+
+simple_iptables_policy "INPUT" do
+  policy "DROP"
+end
+
+# http://serverfault.com/questions/356282/cannot-ping-outside-network-with-these-ip-rules
+simple_iptables_rule "system" do
+  rule "-m state --state ESTABLISHED,RELATED "
+  jump "ACCEPT"
+end
+
+# Allow all traffic on the loopback device
+simple_iptables_rule "system" do
+  rule "--in-interface lo"
+  jump "ACCEPT"
+end
+
+simple_iptables_rule "ssh" do
+  rule "--proto tcp --dport 22"
+  jump "ACCEPT"
+end
+
+simple_iptables_rule "http" do
+  rule [ "--proto tcp --dport 80",
+         "--proto tcp --dport 443" ]
+  jump "ACCEPT"
+end
