42f5dc8fd91536ba0c25cf7bd9bad673f833fb74
niedhui <dianhui.nie@gmail.com>
2013-08-27 15:43:37 +0800

    ulimit

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -3,16 +3,16 @@ package 'vim'
 
 # create deployer user
 user node[:user][:name] do
-password node[:user][:password]
-gid 'admin'
-home "/home/#{node[:user][:name]}"
-supports manage_home: true
-shell "/bin/bash"
+  password node[:user][:password]
+  gid 'admin'
+  home "/home/#{node[:user][:name]}"
+  supports manage_home: true
+  shell "/bin/bash"
 end
 
 template "/home/#{node[:user][:name]}/.gemrc" do
-source "gemrc"
-owner node[:user][:name]
+  source "gemrc"
+  owner node[:user][:name]
 end
 
 
@@ -20,22 +20,24 @@ end
 include_recipe "rbenv::default"
 include_recipe "rbenv::ruby_build"
 
-rbenv_ruby '2.0.0-p247' do
-global true
-end
-rbenv_gem 'ruby-shadow' do
-source 'http://ruby.taobao.org'
-end
-rbenv_gem 'bundler' do
-source 'http://ruby.taobao.org'
+rbenv_ruby File.expand_path('../../../../ruby-2.0.0-p247', __FILE__) do
+  global true
 end
 
-execute "gem sources --remove http://rubygems.org"
+rbenv_gem 'ruby-shadow' do
+  source 'http://ruby.taobao.org'
+end
+
+rbenv_gem 'bundler' do
+  source 'http://ruby.taobao.org'
+end
+
+#TODO: permissions
+execute "gem sources --remove https://rubygems.org/"
 execute "gem sources --add http://ruby.taobao.org"
 
 # install nginx
 include_recipe "nginx::source"
-
 
 ## iptables
 include_recipe "simple_iptables::default"
@@ -65,4 +67,21 @@ simple_iptables_rule "http" do
   rule [ "--proto tcp --dport 80",
          "--proto tcp --dport 443" ]
   jump "ACCEPT"
+end
+
+# ulimit
+include_recipe "ulimit::default"
+
+ulimit_domain "*" do
+  filename 'all.conf'
+  rule do
+    item :nofile
+    type :hard
+    value 51200
+  end
+  rule do
+    item :nofile
+    type :soft
+    value 51200
+  end
 end
